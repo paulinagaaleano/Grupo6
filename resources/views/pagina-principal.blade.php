6f31b515b3acd1c2521c2d48e28c951f5be21738
brenda <bresque12@gmail.com>
2026-04-15 22:02:10 -0300

    Modificaciones vendor y env y carrusel

--- resources/views/pagina-principal.blade.php
+++ resources/views/pagina-principal.blade.php
@@ -28,13 +28,13 @@
 <div id="carouselAura" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="2000">
-            <img src="/img/car2.png" class="d-block w-100" alt="Promoción 1" style="height: 500px; object-fit: cover;">
+            <img src="/img/Carrusel/car2.png" class="d-block w-100" alt="Promoción 1" style="height: 500px; object-fit: cover;">
         </div>
         <div class="carousel-item" data-bs-interval="2000">
-            <img src="/img/car1.png" class="d-block w-100" alt="Promoción 2" style="height: 500px; object-fit: cover;">
+            <img src="/img/Carrusel/car1.png" class="d-block w-100" alt="Promoción 2" style="height: 500px; object-fit: cover;">
         </div>
         <div class="carousel-item" data-bs-interval="2000">
-            <img src="/img/car3.png" class="d-block w-100" alt="Promoción 2" style="height: 500px; object-fit: cover;">
+            <img src="/img/Carrusel/car3.png" class="d-block w-100" alt="Promoción 3" style="height: 500px; object-fit: cover;">
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselAura" data-bs-slide="prev">
