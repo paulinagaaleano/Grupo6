fb98f8deefdd886d9f50c2774eb9426e6daa1dde
paulinagaaleano <patricio.aguirre@25watts.com.ar>
2026-06-10 01:25:15 -0300

    sincronizacion de tablas

--- database/migrations/2026_06_03_181415_create_ventas_cabecera_table.php
+++ database/migrations/2026_06_03_181415_create_ventas_cabecera_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('ventas_cabecera', function (Blueprint $table) {
             $table->id();
             $table->timestamp('fecha_venta')->nullable();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('usuarios')->onDelete('cascade');
             $table->string('estado')->default('carrito');
             // 'carrito' = en proceso | 'confirmado' = compra realizada
             $table->decimal('total', 10, 2)->default(0);
